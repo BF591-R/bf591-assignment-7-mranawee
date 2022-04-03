0938406ca37e006ef9ffa836497d1c8677adc764
mranawee <98359567+mranawee@users.noreply.github.com>
2022-04-03 12:10:17 -0400

    submission

--- app.R
+++ app.R
@@ -7,11 +7,41 @@
 library(shiny)
 library(ggplot2)
 library(colourpicker) # you might need to install this.
+library(dplyr)
 
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-    
+    fileInput("file", "Please select a DESeq data CSV file" , accept = ".csv"), #, placeholder = "deseq_res.csv"),
+    radioButtons('button1', 'Please select a variable to plot on your x-axis', 
+                  c('baseMean',
+                    'log2FoldChange',
+                    'lfcSE',
+                    'stat',
+                    'pvalue',
+                    'padj')),
+    radioButtons('button2', 'Please select a variable to plot on your y-axis', 
+                   c('baseMean',
+                   'log2FoldChange',
+                   'lfcSE',
+                   'stat',
+                   'pvalue',
+                   'padj')),
+    colourInput('col1', 'Choose a color for points below your adjusted p-value threshold', 'red'), #for plotting
+    colourInput('col2', 'Choose color for points above your adjusted p-value threshold', 'blue'),#for what meets p-value threshold
+    sliderInput('slider', 'Select the magnitude of adjusted p-value you would like to color on the graph', -300, 0, -10),
+    submitButton('Submit', icon('sync'), '400px'),
+    mainPanel(
+      tabsetPanel(
+        tabPanel('Differential Expression Data Table',
+          
+          tableOutput('table')
+        ),
+        tabPanel('Differential Expression Data Plot',
+          plotOutput('volcano')
+        )
+      )
+    )
 )
 
 # Define server logic required to draw a histogram
@@ -26,8 +56,8 @@ server <- function(input, output, session) {
     #' our case, look for the uploaded file's datapath argument and load it with 
     #' read.csv. Return this data frame in the normal return() style.
     load_data <- reactive({
-        
-        return()
+        req(input$file)
+        return(read.csv(input$file$datapath, header=TRUE, sep=","))  
     })
     
     #' Volcano plot
@@ -50,11 +80,21 @@ server <- function(input, output, session) {
     #' !!sym() may be required to access column names in ggplot aes().
     #'
     #' @examples volcano_plot(df, "log2fc", "padj", -100, "blue", "taupe")
-    volcano_plot <-
-        function(dataf, x_name, y_name, slider, color1, color2) {
-
-            return()
+    volcano_plot <- function(dataf, x_name, y_name, slider, color1, color2) {
+          dataf$cutoff[dataf$pvalue < 10^slider] <- 'BELOW PADJ MAGNITUDE'
+          dataf$cutoff[dataf$pvalue > 10^slider] <- 'ABOVE PADJ MAGNITUDE'
+          plt <- dataf %>%
+            ggplot() +
+            geom_point(aes(x=!!sym(x_name), y=-log10(!!sym(y_name)), color = cutoff)) + #volcano points
+            xlab(x_name) +
+            ylab(paste('-log10',y_name, sep = ' ')) +
+            scale_color_manual(values=c(color1, color2)) +
+            ggtitle('Volcano Plot of DeSeq Differential Expression Results') +
+            theme_dark()
+            
+            return(plt)
         }
+  
     
     #' Draw and filter table
     #'
@@ -76,17 +116,19 @@ server <- function(input, output, session) {
     #'gene1 11690.780   9.852926 0.2644650  37.25607 8.45125e-304 1.54472e-299
     #'gene2  3550.435  -6.183714 0.1792708 -34.49369 9.97262e-261 9.11398e-257
     draw_table <- function(dataf, slider) {
-        
-        return()
+      filt_dataf <- filter(dataf, padj< 10^slider) %>%  #dataf[!(dataf$padj > 10^slider),]
+        mutate(pvalue = formatC(.$pvalue, digits = 4, format = 'e'),
+               padj = formatC(.$padj, digits = 4, format = 'e'))
+      return(filt_dataf)
     }
     
     #' These outputs aren't really functions, so they don't get a full skeleton, 
     #' but use the renderPlot() and renderTabel() functions to return() a plot 
     #' or table object, and those will be displayed in your application.
-    output$volcano <- NULL # replace this NULL
+    output$volcano <- renderPlot(volcano_plot(load_data(), input$button1, input$button2, input$slider, input$col1, input$col2)) # replace this NULL
     
     # Same here, just return the table as you want to see it in the web page
-    output$table <- NULL # replace this NULL
+    output$table <- renderTable(draw_table(load_data(), input$slider)) # replace this NULL
 }
 
 # Run the application
